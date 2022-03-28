93cde94edac3a234a77b7aee6c7b4ca9422629a3
Anderson Molero <andermolero@gmail.com>
2022-03-28 13:29:25 -0400

    added pokemonlist

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+let pokemonList = [
+    {
+        name: "Charmander",
+        number: "1",
+        height: "2.0",
+        types: ["fire"],
+        weaknesses: ["water", "ground", "rock"],
+    },
+    {
+        name: "Wartortle",
+        number: "2",
+        height: "3.03",
+        types: ["water"],
+        weaknesses: ["grass", "eletric"],
+    },
+    {
+        name: "Ivysaur",
+        number: "3",
+        height: "3.03",
+        types: ["fire", "grass"],
+        weaknesses: ["fire", "psychic", "flying", "ice"],
+    },
+];
+console.log(pokemonList);
